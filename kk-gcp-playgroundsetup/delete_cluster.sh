08a439a9fc3a37836e2c3196b8560d394471c0bd
awesomepras <prasana@alienfrompondy.com>
2023-10-21 00:59:26 -0400

    updated script for deletion

--- kk-gcp-playgroundsetup/delete_cluster.sh
+++ kk-gcp-playgroundsetup/delete_cluster.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 echo "CLEANUP:::"
 CLUSTER_NAME=$(gcloud container clusters list --format=text |grep -w "^name:" |awk '{print $2}')
+COMPUTE_REGION=$(gcloud config list compute/region --format=flattened |awk '{print $2}')
 ZONE=$(gcloud config list compute/zone --format=flattened |awk '{print $2}')
-gcloud container clusters delete $CLUSTER_NAME -z=$ZONE
+
+gcloud container clusters delete $CLUSTER_NAME --location=$COMPUTE_REGION
 
 echo "ALL DONE"
-read -p "remove gcloud configuration?:(y/n)" : y/n
-case $y/n in 
+read -p "remove gcloud configuration?:(y/n)" yn
+case $yn in 
   y)
-    gcloud auth auth revoke --all --quiet
+    k config get-contexts
+    k config delete-context $(k config get-clusters  |grep -v NAME)
+    gcloud auth revoke --all --quiet
     ;;
   n)
     gcloud auth list
