1be317f0dd3564225674c2c4df9975d581dd6108
awesomepras <prasana@alienfrompondy.com>
2024-02-25 22:48:26 -0500

    added autologin and updated gcloud cmd

--- kk-gcp-playgroundsetup/create_cluster_in_gcp.sh
+++ kk-gcp-playgroundsetup/create_cluster_in_gcp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "If you have not done already, authenticate to gcloud"
+ gcloud auth login --no-launch-browser
+
 echo "........"
 read -p "continue"
 # ---------------------------------------------
@@ -16,13 +18,13 @@ ZONE=$(gcloud config list compute/zone --format=flattened |awk '{print $2}')
 # disk size limit is for kodekloud env max size =50GB for all machines 
 read -p "Enter cluster name: (eg: cluster-1)" CLUSTER_NAME
 if [[ $CLUSTER_NAME == "" ]]; then CLUSTER_NAME=cluster-1; fi
-gcloud container clusters create "$CLUSTER_NAME" \
---release-channel "stable"   --machine-type "e2-medium"  \
---image-type "COS_CONTAINERD" --disk-type "pd-standard" \
---disk-size "10" --num-nodes "3" --enable-ip-alias \
-# --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver,ConfigConnector \
---workload-pool=$PROJECT_ID.svc.id.goog \
---region "$COMPUTE_REGION"  --node-locations "$COMPUTE_REGION-a"
+gcloud beta container --project "$PROJECT_ID"  clusters create "$CLUSTER_NAME" \
+ -release-channel "stable"   --machine-type "e2-medium"  \
+ -image-type "COS_CONTAINERD" --disk-type "pd-standard" \
+ -disk-size "10" --num-nodes "3" --enable-ip-alias \
+ -addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver,ConfigConnector,HttpLoadBalancing,GcePersistentDiskCsiDriver  \
+ -workload-pool=$PROJECT_ID.svc.id.goog \
+ -region "$COMPUTE_REGION"  --node-locations "$COMPUTE_REGION-a"
 
 gcloud container clusters list
 read -p "CLuster created and healthy"
